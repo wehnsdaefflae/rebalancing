0629c775af052f1f69567be232a9de47f9bb1323
Mark <wernsdorfer@gmail.com>
2018-06-27 13:26:06 +0200

    started test implementation, moved config into jsonfile

--- source/dtw/main.py
+++ source/dtw/main.py
@@ -43,10 +43,9 @@ def fit_test_data():
     print(msg.format(cur_a, cur_b, offset, overlap, error, offset))
 
 
-def batch():
-    start_date = datetime.datetime(2018, 6, 1, 0, 0, 0, tzinfo=datetime.timezone.utc)
-    end_date = datetime.datetime(2018, 6, 7, 0, 0, 0, tzinfo=datetime.timezone.utc)
-    interval_minutes = 10
+def train_dtw():
+    # "2018-06-01_00:00:00_UTC"
+    # "2018-06-07_00:00:00_UTC"
 
     parameters = {"overlap": True,
                   "normalized": True,
@@ -57,8 +56,11 @@ def batch():
 
     with open("config.json", mode="r") as file:
         config = json.load(file)
-    source_dir = config["data_path"]     # "../../data/binance/23Jun2017-23Jun2018-1m/"
-    target_dir = config["target_path"]  # "../../results/dtw/2018-06-25/"
+    source_dir = config["data_dir"]     # "../../data/binance/23Jun2017-23Jun2018-1m/"
+    target_dir = config["target_dir"]  # "../../results/dtw/2018-06-25/"
+    interval_minutes = config["interval_minutes"]
+    start_date = datetime.datetime.strptime(config["start_date"], "%Y-%m-%d_%H:%M:%S_%Z")
+    end_date = datetime.datetime.strptime(config["end_date"], "%Y-%m-%d_%H:%M:%S_%Z")
 
     all_pairs = [os.path.splitext(f)[0] for f in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir, f))]
     all_pairs = sorted(x for x in all_pairs if x[-3:] == "ETH")
@@ -98,14 +100,13 @@ def batch():
 
 
 def single_run():
-    start_date = datetime.datetime(2018, 6, 1, 0, 0, 0, tzinfo=datetime.timezone.utc)
-    end_date = datetime.datetime(2018, 6, 7, 0, 0, 0, tzinfo=datetime.timezone.utc)
-    interval_minutes = 10
-
     with open("config.json", mode="r") as file:
         config = json.load(file)
-    source_dir = config["data_path"]     # "../../data/binance/23Jun2017-23Jun2018-1m/"
-    target_dir = config["target_path"]  # "../../results/dtw/2018-06-25/"
+    source_dir = config["data_dir"]     # "../../data/binance/23Jun2017-23Jun2018-1m/"
+    target_dir = config["target_dir"]  # "../../results/dtw/2018-06-25/"
+    interval_minutes = config["interval_minutes"]
+    start_date = datetime.datetime.strptime(config["start_date"], "%Y-%m-%d_%H:%M:%S_%Z")
+    end_date = datetime.datetime.strptime(config["end_date"], "%Y-%m-%d_%H:%M:%S_%Z")
 
     parameters = {"overlap": True,
                   "normalized": True,
@@ -117,9 +118,33 @@ def single_run():
     fit_exchange_rates("ADAETH", "ADXETH", start_date, end_date, interval_minutes, parameters, source_dir)
 
 
+def test_dtw():
+    with open("config.json", mode="r") as file:
+        config = json.load(file)
+    source_dir = config["data_dir"]         # "../../data/binance/23Jun2017-23Jun2018-1m/"
+    target_dir = config["target_dir"]       # "../../results/dtw/2018-06-25/"
+    interval = config["interval_minutes"]   # 10
+
+    results = []
+    with open(target_dir + "results.csv", mode="r") as file:
+        line = file.readline()
+        header = line[1:-1].split("\t")
+        for each_line in file:
+            row = each_line[1:-1].split("\t")
+            try:
+                each_result = {_h: float(_c) if not _h.startswith("cur") else _c for _h, _c in zip(header, row)}
+            except ValueError:
+                continue
+            results.append(each_result)
+
+    for each_result in results:
+        cur_a, cur_b = each_result["currency_a"], each_result["currency_b"]
+        error, overlap, offset = each_result["error"], each_result["overlap"], each_result["offset"]
+
+
 def main():
     # single_run()
-    batch()
+    train_dtw()
 
 
 if __name__ == "__main__":
