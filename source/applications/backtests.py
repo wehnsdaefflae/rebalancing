c6c9eb8986457b60790e9c4be0199bc395b1ccde
Mark <wernsdorfer@gmail.com>
2018-07-05 16:08:33 +0200

    trading bot refactoring

--- source/applications/backtests.py
+++ source/applications/backtests.py
@@ -1,10 +1,9 @@
 import datetime
-from typing import Dict, Tuple, Generic, Collection, Callable, Any
+from typing import Dict, Tuple, Generic, Collection, Callable, Any, Sequence, Optional, List
 
 from matplotlib import pyplot
 from matplotlib.axes import Axes
 
-from source.data.data_generation import series_generator
 from source.tactics.signals.signals import TradingSignal, SIGNAL_INPUT, RATE_INFO, SymmetricChannelSignal
 
 
@@ -12,7 +11,7 @@ PORTFOLIO_INFO = Dict[str, float]
 TECH_INFO = Tuple[RATE_INFO, PORTFOLIO_INFO]
 SIGNALS_OUTPUT = Dict[str, float]
 SIGNALS_INPUT = Dict[str, SIGNAL_INPUT]
-REBALANCER = Callable[TECH_INFO, PORTFOLIO_INFO]
+BALANCING = Callable[TECH_INFO, PORTFOLIO_INFO]
 RATE_GENERATOR = Callable[Any, Tuple[datetime.datetime, float]]  # covers generators?!
 
 # parameters of data generator
@@ -25,11 +24,10 @@ source_dir: str
 
 class TradingBot(Generic[SIGNAL_INPUT]):
     def __init__(self,
-                 portfolio: PORTFOLIO_INFO,
                  signals: Dict[str, TradingSignal],
                  data_sources: Dict[str, RATE_GENERATOR],
                  risk: float = .5,
-                 rebalancer: REBALANCER = None,
+                 balancing: BALANCING = None,
                  trading_fee_percentage: float = .0025,
                  base_asset: str = "ETH",
                  min_base_transaction_volume: float = .025):
@@ -37,38 +35,20 @@ class TradingBot(Generic[SIGNAL_INPUT]):
         if base_asset not in data_sources:
             raise ValueError("No data source for base asset.")
 
+        if base_asset in signals:
+            raise ValueError("No bas asset signal for now.")
+
         self.signals = signals
         self.data_sources = data_sources
         self.base_asset = base_asset
         self.risk = risk
-        self.rebalancer = rebalancer
+        self.balancing = balancing
         self.trading_factor = 1. - trading_fee_percentage
         self.min_base_transaction_volume = min_base_transaction_volume
-        self.rates = None
-        self.current_time = None
 
-        self.asset_development = []  # Sequence[Tuple[datetime.datetime, Dict[str, float]]]
+        self.asset_development = []     # type: List[Tuple[datetime.datetime, Dict[str, float]]]
 
-    def _log_portfolio(self, time: datetime.datetime, portfolio: PORTFOLIO_INFO):
-        new_entry = time, dict(portfolio)
-        self.asset_development.append(new_entry)
-
-    @staticmethod
-    def _get_delta(portfolio: PORTFOLIO_INFO, base_asset: str, signals: SIGNALS_OUTPUT, ratio: float) -> PORTFOLIO_INFO:
-        base_volume = portfolio.get(base_asset, 0.)
-        pos_sum = sum(_v for _v in signals.values() if _v < 0.)
-        delta = dict()
-        for _k, _s in signals.items():
-            if _s < 0.:
-                asset_volume = portfolio.get(_k, -1.)
-                if 0. < asset_volume:
-                    delta[_k] = ratio * asset_volume * _s
-            elif 0. < _s:
-                delta[_k] = ratio * base_volume * _s / pos_sum
-
-        return delta
-
-    def __get_rates(self) -> RATE_INFO:
+    def __get_rates(self) -> Tuple[datetime.datetime, RATE_INFO]:
         rate_info = dict()
         dates = set()
         for each_asset, each_source in self.data_sources.items():
@@ -76,9 +56,16 @@ class TradingBot(Generic[SIGNAL_INPUT]):
             dates.add(each_date)
             if 1 < len(dates):
                 raise ValueError("Data sources returned inconsistent time information.")
-            self.current_time, = dates
             rate_info[each_asset] = each_rate
-        return rate_info
+        current_time, = dates
+        return current_time, rate_info
+
+    def _get_portfolio(self):
+        raise NotImplementedError()
+
+    def __log_portfolio(self, time: datetime.datetime, portfolio: PORTFOLIO_INFO):
+        new_entry = time, dict(portfolio)
+        self.asset_development.append(new_entry)
 
     def _get_signals_input(self) -> SIGNALS_INPUT:
         raise NotImplementedError()
@@ -93,43 +80,84 @@ class TradingBot(Generic[SIGNAL_INPUT]):
             tendencies[each_asset] = each_output
         return tendencies
 
-    def run(self):
-        while True:
-            try:
-                self.rates = self.__get_rates()
-            except StopIteration as e:
-                break
+    @staticmethod
+    def __get_delta(portfolio: PORTFOLIO_INFO, base_asset: str, signals: SIGNALS_OUTPUT, risk: float) -> PORTFOLIO_INFO:
+        base_volume = portfolio.get(base_asset, 0.)
+        pos_sum = sum(_v for _v in signals.values() if .0 < _v)
+        delta = dict()
+        for _k, _s in signals.items():
+            if _s < 0.:     # sell
+                asset_volume = portfolio.get(_k, -1.)
+                if 0. < asset_volume:
+                    delta[_k] = risk * asset_volume * _s
 
-            signal_inputs = self._get_signals_input()   # internet problems?
-            portfolio = self._get_portfolio()           # no access to api?
-            signals = self._get_signals(signal_inputs)
+            elif 0. < _s:   # buy
+                delta[_k] = risk * base_volume * _s / pos_sum  # * asset_rate
 
-            self._log_portfolio(self.current_time, portfolio)
+        return delta
 
-            portfolio_delta = TradingBot._get_delta(portfolio, self.base_asset, signals, self.risk)
-            if self.rebalancer is not None:
-                portfolio_target = self._change_portfolio(portfolio, portfolio_delta, no_fees=True)
-                tech_info = portfolio_target, self.rates
-                portfolio_delta = self.rebalancer(tech_info)
-
-            portfolio_target = self._change_portfolio(portfolio, portfolio_delta)
-            self._redistribute(portfolio, portfolio_target)
-
-    def _change_portfolio(self, portfolio: PORTFOLIO_INFO, delta: PORTFOLIO_INFO, no_fees: bool = False):
+    def __change_portfolio(self, portfolio: PORTFOLIO_INFO, delta: PORTFOLIO_INFO, no_fees: bool = False):
         all_assets = set(portfolio.keys()) | set(delta.keys())
         if no_fees:
             return {_k: (portfolio.get(_k, .0) + delta.get(_k, .0)) for _k in all_assets}
         return {_k: (portfolio.get(_k, .0) + delta.get(_k, .0)) * self.trading_factor ** 2 for _k in all_assets}
 
-    def _redistribute(self, current_portfolio: PORTFOLIO_INFO, target_portfolio: PORTFOLIO_INFO):
-        # call self._transfer
-        pass
+    def __redistribute(self, delta: PORTFOLIO_INFO, rates: RATE_INFO):
+        if self.base_asset in delta:
+            raise ValueError("No base asset transactions!")
 
-    def _transfer(self, source_value: float, source_asset: str, target_value: float, target_asset: str):
+        for each_asset, each_value in delta.items():
+            if each_value < 0.:
+                asset_rate = rates[each_asset]
+                base_value = each_value / asset_rate
+                if -base_value < self.min_base_transaction_volume:
+                    continue
+                self._transfer(-each_value, each_asset, self.base_asset, asset_rate)
+
+            elif 0. < each_value:
+                asset_rate = rates[each_asset]
+                base_value = each_value / asset_rate
+                if base_value < self.min_base_transaction_volume:
+                    continue
+                self._transfer(base_value, self.base_asset, each_asset, asset_rate)
+
+    def _transfer(self, source_value: float, source_asset: str, target_asset: str, rate: float):
         raise NotImplementedError()
 
-    def _get_portfolio(self):
+    def _wait(self):
         raise NotImplementedError()
+
+    def run(self):
+        while True:
+            try:
+                time, rates = self.__get_rates()
+            except StopIteration as e:
+                print(e)
+                break
+
+            try:
+                portfolio = self._get_portfolio()
+                self.__log_portfolio(time, portfolio)
+            except Exception as e:
+                print(e)
+                continue
+
+            try:
+                signal_inputs = self._get_signals_input()
+            except Exception as e:
+                print(e)
+                continue
+
+            signals = self._get_signals(signal_inputs)
+
+            portfolio_delta = TradingBot.__get_delta(portfolio, self.base_asset, signals, self.risk)
+            if self.balancing is not None:
+                portfolio_target = self.__change_portfolio(portfolio, portfolio_delta, no_fees=True)
+                tech_info = portfolio_target, rates
+                portfolio_delta = self.balancing(tech_info)
+
+            self.__redistribute(portfolio_delta, rates)
+            self._wait()
 
     def plot_stack(self, axis: Axes):
         all_assets = sorted(set(_x for _y in self.asset_development for _x in _y[1].keys()))
