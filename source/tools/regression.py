2c814826de11e2f137a2ded408d6b4a7dbfac417
Mark <wernsdorfer@gmail.com>
2018-08-07 08:11:54 +0200

    multiple linear regression prototype

--- source/tools/regression.py
+++ source/tools/regression.py
@@ -51,11 +51,11 @@ class Regressor:
     def _get_parameters(self) -> Tuple[float, float]:
         a = 0. if self.var_x == 0. else self.cov_xy / self.var_x
         t = self.mean_y - a * self.mean_x
-        return a, t
+        return t, a
 
     def output(self, x: float) -> float:
-        a, t = self._get_parameters()
-        return x * a + t
+        x0, x1 = self._get_parameters()
+        return x0 + x1 * x
 
 
 class MultiRegressor:
@@ -82,20 +82,15 @@ class MultiRegressor:
 
         return 1. - min(1., d / self.var_y)
 
-    def output(self, x: Tuple[float, ...]) -> float:
-        assert len(x) == self.dim
-        p = self._get_parameters()
-        return sum(x if _i == self.dim - 1 else x * _p for (_i, _p) in enumerate(p))
-
     def fit(self, x: Tuple[float, ...], y: float):
         assert len(x) == self.dim
 
-        dx = [_x - self.mean_x[_i] for (_i, _x) in enumerate(x)]
+        dx = [_x - _mean_x for (_x, _mean_x) in zip(x, self.mean_x)]
         dy = y - self.mean_y
 
-        self.var_x = [(self.drag * self.var_x[_i] + _dx ** 2) / (self.drag + 1) for (_i, _dx) in enumerate(dx)]
+        self.var_x = [(self.drag * _var_x + _dx ** 2) / (self.drag + 1) for (_var_x, _dx) in zip(self.var_x, dx)]
         self.var_y = (self.drag * self.var_y + dy ** 2) / (self.drag + 1)
-        self.cov_xy = [(self.drag * self.cov_xy[_i] + _dx * dy) / (self.drag + 1) for (_i, _dx) in enumerate(dx)]
+        self.cov_xy = [(self.drag * _cov_xy + _dx * dy) / (self.drag + 1) for (_cov_xy, _dx) in zip(self.cov_xy, dx)]
 
         if self.initial:
             self.mean_x = x
@@ -103,31 +98,57 @@ class MultiRegressor:
             self.initial = False
 
         else:
-            self.mean_x = [(self.drag * self.mean_x[_i] + _x) / (self.drag + 1) for (_i, _x) in enumerate(x)]
+            self.mean_x = [(self.drag * _mean_x + _x) / (self.drag + 1) for (_mean_x, _x) in zip(self.mean_x, x)]
             self.mean_y = (self.drag * self.mean_y + y) / (self.drag + 1)
 
-    def _get_parameters(self) -> Tuple[float, ...]:
+    def output(self, x: Tuple[float, ...]) -> float:
+        assert len(x) == self.dim
+        xn, x0 = self._get_parameters()
+        return x0 + sum(_x * _xn for (_x, _xn) in zip(x, xn))
 
-        a = 0. if self.var_x == 0. else self.cov_xy / self.var_x
-        t = self.mean_y - a * self.mean_x
-        return a, t
+    def _get_parameters(self) -> Tuple[Tuple[float, ...], float]:
+        xn = tuple(0. if _var_x == 0. else _cov_xy / _var_x for (_cov_xy, _var_x) in zip(self.cov_xy, self.var_x))
+        x0 = self.mean_y - sum(_xn * _mean_x for (_xn, _mean_x) in zip(xn, self.mean_x))
+        return xn, x0
 
 
 if __name__ == "__main__":
-    f = lambda _x: .3 * _x - 7.
-    x = range(20)
-    y = [f(_x) + (random.random() - .5) * 2. for _x in x]
+    from mpl_toolkits.mplot3d import Axes3D
+    # https://stackoverflow.com/questions/48335279/given-general-3d-plane-equation-how-can-i-plot-this-in-python-matplotlib
+    # https://stackoverflow.com/questions/36060933/matplotlib-plot-a-plane-and-points-in-3d-simultaneously
+    f = lambda _x, _y: .3 * _x + -.4 * _y - 7.
+    x = []
+    y = []
+    z = []
+    for _x in range(20):
+        for _y in range(20):
+            v = f(_x, _y) + (4. * random.random() - 2.)
+            x.append(_x)
+            y.append(_y)
+            z.append(v)
+
+    fig = pyplot.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for _x, _y, _z in zip(x, y, z):
+        ax.scatter(_x, _y, _z)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+    pyplot.show()
+
+    exit()
+
     pyplot.plot(x, y, label="original")
 
-    regressor = Regressor(10)
+    regressor = MultiRegressor(1, 10)
     for _x, _y in zip(x, y):
-        regressor.fit(_x, _y)
+        regressor.fit((_x, ), _y)
 
     in_value = 1.
-    o = regressor.output(in_value)
-    s = regressor.sim(in_value, o*.9)
+    o = regressor.output((in_value, ))
+    s = regressor.sim((in_value, ), o * .9)
 
-    pyplot.plot(x, [regressor.output(_x) for _x in x], label="fitted")
+    pyplot.plot(x, [regressor.output((_x, )) for _x in x], label="fitted")
 
     pyplot.legend()
     pyplot.show()
