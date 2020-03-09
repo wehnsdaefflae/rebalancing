8d98a4c51bb9f79a99661df700082764db41d27b
Mark <wernsdorfer@gmail.com>
2020-03-09 17:12:38 +0100

    added script to merge binance data

---
+++ source/data/merge_data_binance.py
@@ -0,0 +1,28 @@
+import glob
+import os
+
+
+def main():
+    folder_old = "C:/Users/Mark/PycharmProjects/rebalancing/data/binance/old/"
+    folder_new = "C:/Users/Mark/PycharmProjects/rebalancing/data/binance/new/"
+
+    files = {os.path.basename(x) for folder in (folder_old, folder_new) for x in glob.glob(folder + "*.csv")}
+
+    for each_file_name in sorted(files):
+        print(f"reading {each_file_name:s}...")
+        timestamp_last = -1
+        with open(folder_old + each_file_name, mode="r") as file_old:
+            for line in file_old:
+                timestamp_this = int(line.split("\t", maxsplit=1)[0])
+                timestamp_last = max(timestamp_last, timestamp_this)
+
+        print(f"appending {each_file_name:s}...")
+        with open(folder_new + each_file_name, mode="r") as file_new, open(folder_old + each_file_name, mode="a") as file_old:
+            for line in file_new:
+                timestamp_this = int(line.split("\t", maxsplit=1)[0])
+                if timestamp_last < timestamp_this:
+                    file_old.write(line)
+
+
+if __name__ == "__main__":
+    main()
