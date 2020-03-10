34ba7898f8c94497800f6001ee7fd8567715466d
Mark <wernsdorfer@gmail.com>
2020-03-10 19:56:59 +0100

    started dynamic programming optimal trading

--- source/tactics/optimal_trading.py
+++ source/tactics/optimal_trading.py
@@ -73,5 +73,78 @@ def main():
     # pyplot.show()
 
 
+def main_new():
+    random.seed(235235)
+
+    size = 10
+    seq_ass = get_sequence(53.5, size)
+    seq_sec = get_sequence(12.2, size)
+
+    ass = [False for _ in range(size)]
+    sec = [False for _ in range(size)]  # false is hold, true is sell
+
+    val_ass = [-1. for _ in range(size)]
+    val_sec = [-1. for _ in range(size)]
+
+    val_sec[0] = 0.
+    val_sec[0] = 1.
+
+    ass[0] = True
+    sec[0] = True
+
+    # keep two alternative accumulative value sequences
+    # split on each switch, keep alternative, discard worse sequence
+    # remember actions, not states
+
+    for i in range(1, size - 1):
+        change_sec = math.log(seq_sec[i+1] / seq_sec[i])
+        change_ass = math.log(seq_ass[i+1] / seq_ass[i])
+
+        """
+        if sec[i-1] and ass[i-1]:
+            pass
+        """
+
+        if ass[i-1]:
+            tmp_ass = val_ass[i-1] * change_ass
+            tmp_sec = val_sec[i-1] * change_sec
+            if tmp_ass < tmp_sec:
+                # terminate ass sequence
+                # continue sec sequence
+                pass
+            else:
+                # continue ass sequence
+                # terminate sec sequence
+                pass
+
+        if sec[i-1]:
+            pass
+
+        if not ass[i-1] and not sec[i-1]:
+            val_sec[i] = val_sec[i-1] * change_sec
+            val_ass[i] = val_ass[i-1] * change_ass
+
+
+    sec[-1] = True
+    ass[-1] = False
+    change_sec = seq_sec[-1] / seq_sec[-2]
+    val[-1] = val[-2] * change_sec
+
+    print("tick  " + "".join(f"{i: 7d}" for i in range(size)))
+    print()
+    print("ass_v " + "".join(f"  {x:5.2f}" for x in seq_ass))
+    print("sec_v " + "".join(f"  {x:5.2f}" for x in seq_sec))
+    print()
+    print("ass   " + "".join(f"{str(x)[0]:>7s}" for x in ass))
+    print("sec   " + "".join(f"{str(x)[0]:>7s}" for x in sec))
+    print()
+    print("value " + "".join(f"  {x:5.2f}" for x in val))
+
+    r = [0. for _ in range(size)]
+
+    # pyplot.plot(seq)
+    # pyplot.show()
+
+
 if __name__ == "__main__":
     main()
