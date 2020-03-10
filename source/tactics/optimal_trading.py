d3285ebc651e8427b83acfc123845d7188e5b3e0
Mark <wernsdorfer@gmail.com>
2020-03-10 23:58:20 +0100

    optimal investment seems to work

--- source/tactics/optimal_trading.py
+++ source/tactics/optimal_trading.py
@@ -22,125 +22,80 @@ def main():
     random.seed(235235)
 
     size = 10
-    seq_sec = get_sequence(12.2, size)
-    seq_ass = get_sequence(53.5, size)
-
-    sec = [False for _ in range(size)]
-    ass = [False for _ in range(size)]
-    val = [-1. for _ in range(size)]
-
-    sec[0] = True
-    ass[0] = False
-    val[0] = 1.
-
-    for i in range(size - 1):
-        change_sec = seq_sec[i+1] / seq_sec[i]
-        change_ass = seq_ass[i+1] / seq_ass[i]
-
-        if change_sec < change_ass and sec[i]:
-            ass[i+1] = True
-            sec[i+1] = False
-            val[i+1] = val[i] * change_ass
-
-        elif change_ass < change_sec and ass[i]:
-            ass[i+1] = False
-            sec[i+1] = True
-            val[i+1] = val[i] * change_sec
-
-        else:
-            ass[i+1] = ass[i]
-            sec[i+1] = sec[i]
-            val[i+1] = val[i]
-
-    sec[-1] = True
-    ass[-1] = False
-    change_sec = seq_sec[-1] / seq_sec[-2]
-    val[-1] = val[-2] * change_sec
-
-    print("tick  " + "".join(f"{i: 7d}" for i in range(size)))
-    print()
-    print("ass_v " + "".join(f"  {x:5.2f}" for x in seq_ass))
-    print("sec_v " + "".join(f"  {x:5.2f}" for x in seq_sec))
-    print()
-    print("ass   " + "".join(f"{str(x)[0]:>7s}" for x in ass))
-    print("sec   " + "".join(f"{str(x)[0]:>7s}" for x in sec))
-    print()
-    print("value " + "".join(f"  {x:5.2f}" for x in val))
-
-    r = [0. for _ in range(size)]
-
-    # pyplot.plot(seq)
-    # pyplot.show()
-
-
-def main_new():
-    random.seed(235235)
-
-    size = 10
     seq_ass = get_sequence(53.5, size)
     seq_sec = get_sequence(12.2, size)
 
-    ass = [False for _ in range(size)]
-    sec = [False for _ in range(size)]  # false is hold, true is sell
+    origin_ass = [False for _ in range(size-1)]  # [False: held, True: bought]
+    origin_sec = [False for _ in range(size-1)]  # [False: held, True: bought]
 
     val_ass = [-1. for _ in range(size)]
     val_sec = [-1. for _ in range(size)]
 
-    val_sec[0] = 0.
+    val_ass[0] = 0.
     val_sec[0] = 1.
-
-    ass[0] = True
-    sec[0] = True
 
     # keep two alternative accumulative value sequences
     # split on each switch, keep alternative, discard worse sequence
     # remember actions, not states
 
-    for i in range(1, size - 1):
-        change_sec = math.log(seq_sec[i+1] / seq_sec[i])
-        change_ass = math.log(seq_ass[i+1] / seq_ass[i])
+    # todo: generalize to n assets
 
-        """
-        if sec[i-1] and ass[i-1]:
-            pass
-        """
+    for i in range(size - 1):
+        # next sec
+        change_sec = seq_sec[i+1] / seq_sec[i]
 
-        if ass[i-1]:
-            tmp_ass = val_ass[i-1] * change_ass
-            tmp_sec = val_sec[i-1] * change_sec
-            if tmp_ass < tmp_sec:
-                # terminate ass sequence
-                # continue sec sequence
-                pass
+        hold_sec = val_sec[i] * change_sec
+        sell_ass = val_ass[i] * change_sec
+        if hold_sec < sell_ass or i >= size - 2:
+            origin_sec[i] = True           # set origin of sec i + 1 to from ass
+            val_sec[i+1] = sell_ass
+
+        else:
+            origin_sec[i] = False          # set origin of sec i + 1 to from ass
+            val_sec[i+1] = hold_sec
+
+        # next ass
+        change_ass = seq_ass[i+1] / seq_ass[i]
+
+        hold_ass = val_ass[i] * change_ass
+        sell_sec = val_sec[i] * change_ass
+        if hold_ass < sell_sec and i < size - 2:
+            origin_ass[i] = True           # set origin of ass i + 1 to from sec
+            val_ass[i+1] = sell_sec
+
+        else:
+            origin_ass[i] = False          # set origin of ass i + 1 to from ass
+            val_ass[i+1] = hold_ass
+
+    origin = origin_sec
+    storage_inv = ["sec"]
+    for i in range(size - 2, -1, -1):
+        if origin[i]:
+            if storage_inv[-1] == "sec":
+                storage_inv.append("ass")
             else:
-                # continue ass sequence
-                # terminate sec sequence
-                pass
+                storage_inv.append("sec")
+            origin = origin_ass if origin == origin_sec else origin_sec
+        else:
+            storage_inv.append(storage_inv[-1])
 
-        if sec[i-1]:
-            pass
+    storage_path = storage_inv[::-1]
 
-        if not ass[i-1] and not sec[i-1]:
-            val_sec[i] = val_sec[i-1] * change_sec
-            val_ass[i] = val_ass[i-1] * change_ass
-
-
-    sec[-1] = True
-    ass[-1] = False
-    change_sec = seq_sec[-1] / seq_sec[-2]
-    val[-1] = val[-2] * change_sec
-
-    print("tick  " + "".join(f"{i: 7d}" for i in range(size)))
+    print("tick    " + "".join(f"{i: 9d}" for i in range(size)))
     print()
-    print("ass_v " + "".join(f"  {x:5.2f}" for x in seq_ass))
-    print("sec_v " + "".join(f"  {x:5.2f}" for x in seq_sec))
+    print("seq_ass " + "".join(f"    {x:5.2f}" for x in seq_ass))
+    print("seq_sec " + "".join(f"    {x:5.2f}" for x in seq_sec))
     print()
-    print("ass   " + "".join(f"{str(x)[0]:>7s}" for x in ass))
-    print("sec   " + "".join(f"{str(x)[0]:>7s}" for x in sec))
+    print("val_ass " + "".join(f"    {x:5.2f}" for x in val_ass))
+    print("origin  " + "".join(["  initial"] + [f"   bought" if x else "     held" for x in origin_ass]))
     print()
-    print("value " + "".join(f"  {x:5.2f}" for x in val))
+    print("val_sec " + "".join(f"    {x:5.2f}" for x in val_sec))
+    print("origin  " + "".join(["  initial"] + [f"   bought" if x else "     held" for x in origin_sec]))
+    print()
+    print("path    " + "".join(f"{x:>9s}" for x in storage_path[1:]))
+    print()
 
-    r = [0. for _ in range(size)]
+    print(f"final value: {val_sec[-1]:5.2f}")
 
     # pyplot.plot(seq)
     # pyplot.show()
