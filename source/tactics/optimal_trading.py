ce9285fb52a6f3b16c2e4559313723a78d3544af
Mark <wernsdorfer@gmail.com>
2020-03-10 13:49:41 +0100

    first draft of optimal trading, probably still quite wrong

--- source/tactics/optimal_trading.py
+++ source/tactics/optimal_trading.py
@@ -1,33 +1,76 @@
 # https://www.dropbox.com/s/ed5hm4rd0b8bz18/optimal.pdf?dl=0
+import math
 import random
 from typing import Sequence
 
-from matplotlib import pyplot
+# from matplotlib import pyplot
 
 
-def get_sequence(length: int) -> Sequence[float]:
-    random.seed(235235)
-    s = [10.]
+def get_sequence(start_value: float, length: int) -> Sequence[float]:
+    s = [-1. for _ in range(length)]
+    s[0] = start_value
 
-    for _ in range(length - 1):
-        r = random.random() * .1
-        if random.random() < .5 and s[-1] - r >= 0.:
-            s.append(s[-1] - r)
-        else:
-            s.append(s[-1] + r)
+    for i in range(1, length):
+        value_last = s[i-1]
+        r = random.uniform(-.1, .1) * value_last
+        s[i] = value_last + r
 
     return s
 
 
 def main():
-    size = 1000
-    seq = get_sequence(size)
+    random.seed(235235)
 
-    t = [False for _ in seq]
-    r = [0. for _ in seq]
+    size = 10
+    seq_sec = get_sequence(12.2, size)
+    seq_ass = get_sequence(53.5, size)
 
-    pyplot.plot(seq)
-    pyplot.show()
+    sec = [False for _ in range(size)]
+    ass = [False for _ in range(size)]
+    val = [-1. for _ in range(size)]
+
+    sec[0] = True
+    ass[0] = False
+    val[0] = 1.
+
+    for i in range(size - 1):
+        change_sec = seq_sec[i+1] / seq_sec[i]
+        change_ass = seq_ass[i+1] / seq_ass[i]
+
+        if change_sec < change_ass and sec[i]:
+            ass[i+1] = True
+            sec[i+1] = False
+            val[i+1] = val[i] * change_ass
+
+        elif change_ass < change_sec and ass[i]:
+            ass[i+1] = False
+            sec[i+1] = True
+            val[i+1] = val[i] * change_sec
+
+        else:
+            ass[i+1] = ass[i]
+            sec[i+1] = sec[i]
+            val[i+1] = val[i]
+
+    sec[-1] = True
+    ass[-1] = False
+    change_sec = seq_sec[-1] / seq_sec[-2]
+    val[-1] = val[-2] * change_sec
+
+    print("tick  " + "".join(f"{i: 7d}" for i in range(size)))
+    print()
+    print("ass_v " + "".join(f"  {x:5.2f}" for x in seq_ass))
+    print("sec_v " + "".join(f"  {x:5.2f}" for x in seq_sec))
+    print()
+    print("ass   " + "".join(f"{str(x)[0]:>7s}" for x in ass))
+    print("sec   " + "".join(f"{str(x)[0]:>7s}" for x in sec))
+    print()
+    print("value " + "".join(f"  {x:5.2f}" for x in val))
+
+    r = [0. for _ in range(size)]
+
+    # pyplot.plot(seq)
+    # pyplot.show()
 
 
 if __name__ == "__main__":
