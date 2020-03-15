ebc758f5ba59a7ec82e04e48eb538229ea8b4e29
Mark <wernsdorfer@gmail.com>
2020-03-15 13:39:13 +0100

    some refactoring

--- source/tactics/optimal_trading.py
+++ source/tactics/optimal_trading.py
@@ -1,7 +1,8 @@
 # https://www.dropbox.com/s/ed5hm4rd0b8bz18/optimal.pdf?dl=0
 import math
 import random
-from typing import Sequence, Tuple, Callable
+from pprint import pprint
+from typing import Sequence, Tuple, Callable, Generator, Iterable, Iterator
 
 # from matplotlib import pyplot
 from matplotlib import pyplot
@@ -10,7 +11,7 @@ from source.data.merge_csv import merge_generator
 from source.tools.timer import Timer
 
 
-def get_random_sequence(start_value: float, length: int) -> Sequence[float]:
+def get_random_sequence(start_value: float, length: int) -> Iterator[float]:
     s = [-1. for _ in range(length)]
     s[0] = start_value
 
@@ -19,7 +20,7 @@ def get_random_sequence(start_value: float, length: int) -> Sequence[float]:
         r = random.uniform(-.1, .1) * value_last
         s[i] = value_last + r
 
-    return s
+    return (v for v in s)
 
 
 def forward(
@@ -80,66 +81,46 @@ def forward(
     return path, roi
 
 
+def generate_positive_change() -> Generator[float, float, None]:
+    rate_last = yield
+    each_rate = yield
+    while True:
+
+        if each_rate < 0.:
+            change = -1.
+
+        elif 0. >= rate_last:
+            change = -1.
+            rate_last = each_rate
+
+        else:
+            change = each_rate / rate_last
+            rate_last = each_rate
+
+        each_rate = yield change
+
+
+def generate_changes(generator_rates: Iterator[Sequence[float]]) -> Generator[Sequence[float], None, None]:
+    rates_now = next(generator_rates)
+    no_rates = len(rates_now)
+
+    generators_change = tuple(generate_positive_change() for _ in rates_now)
+    for each_change_gen, first_rate in zip(generators_change, rates_now):
+        next(each_change_gen)               # initialize
+        each_change_gen.send(first_rate)    # send first rate
+
+    for rates_now in generator_rates:
+        assert len(rates_now) == no_rates
+        yield tuple(x.send(r) for x, r in zip(generators_change, rates_now))
+
+
 def forward_negative(
-        rates: Sequence[Sequence[float]],
+        rates: Iterator[Sequence[float]],
         fees: Callable[[float, int, int], float] = lambda _amount_from, _asset_from, _asset_to: 0.) -> Tuple[Sequence[int], float]:
-    print(f"forward pass...")
-    no_assets = len(rates)
-    len_sequence, = set(len(x) for x in rates)
-    len_path = len_sequence - 1
 
-    values_objective = [1. for _ in rates]
-    matrix = tuple([] for _ in rates)
+    matrix_change = generate_changes(rates)
 
-    iterations_total = len_path * no_assets * no_assets
-    iterations_done = 0
-
-    for t in range(len_path):
-        rates_now = tuple(x[t] for x in rates)
-        rates_next = tuple(x[t + 1] for x in rates)
-        values_tmp = values_objective[:]
-        for asset_to, (rate_to_now, rate_to_next) in enumerate(zip(rates_now, rates_next)):
-            asset_from = asset_to
-
-            invalid = 0. >= rate_to_next or 0. >= rate_to_now
-            if not invalid:
-                change = rate_to_next / rate_to_now
-
-                value_max = -1.
-                for asset_tmp in range(no_assets):
-                    if Timer.time_passed(2000):
-                        print(f"matrix built by {iterations_done * 100. / iterations_total:5.2f}%...")
-                    iterations_done += 1
-
-                    value_tmp = values_objective[asset_tmp] * (1. if invalid else change)
-                    value_tmp -= fees(value_tmp, asset_tmp, asset_to)
-                    if value_tmp < 0.:
-                        continue
-
-                    if value_max < value_tmp or (value_max == value_tmp and asset_tmp == asset_to):  # additional condition can be removed for fees
-                        asset_from = asset_tmp
-                        value_max = value_tmp
-
-                values_tmp[asset_to] = value_max
-
-            asset_row = matrix[asset_to]
-            asset_row.append(asset_from)
-
-        values_objective = values_tmp
-
-        continue
-
-    print(f"backwards pass...")
-    asset_last, roi = max(enumerate(values_objective), key=lambda x: x[1])
-    path = [asset_last]
-    for i in range(len_path - 1, 0, -1):
-        asset_row = matrix[asset_last]
-        asset_last = asset_row[i]
-        path.insert(0, asset_last)
-        if Timer.time_passed(2000):
-            print(f"finished {(len_path - 1 - i) * 100. / (len_path - 1):5.2f}% of backwards pass...")
-
-    return path, roi
+    return [], -1.
 
 
 def plot_trading(path_trade: Sequence[int], seq_ass: Sequence[float], seq_sec: Sequence[float]):
@@ -242,19 +223,24 @@ def simulate_alternative(
 def fees_debug(amount_from: float, asset_from: int, asset_to: int) -> float:
     if asset_from == asset_to:
         return 0.
-    return amount_from * .01
+    return amount_from * .0
 
 
-def get_random_rates(size: int = 20, no_assets: int = 10) -> Tuple[Sequence[int], Sequence[Sequence[float]]]:
+def get_random_rates(size: int = 20, no_assets: int = 10) -> Iterator[Tuple[int, Sequence[float]]]:
     random.seed(235235)
 
-    return list(range(size)), tuple(
-        get_random_sequence(random.uniform(10., 60.), size)
-        for _ in range(no_assets)
-    )
+    sequences = tuple(get_random_sequence(random.uniform(10., 60.), size) for _ in range(no_assets))
+    return ((i, x) for i, x in enumerate(zip(*sequences)))
 
 
-def get_decreasing_rates(size: int = 20, no_assets: int = 10) -> Tuple[Sequence[int], Sequence[Sequence[float]]]:
+def get_debug_rates() -> Iterator[Tuple[int, Sequence[float]]]:
+    rate_a = 59.69, -1.,   -1., 65.73, 59.37
+    rate_b = 30.06, -1., 27.06, 27.09, 29.13
+
+    return ((i, r) for i, r in enumerate(zip(rate_a, rate_b)))
+
+
+def get_decreasing_rates(size: int = 20, no_assets: int = 10) -> Iterator[Tuple[int, Sequence[float]]]:
     random.seed(235235)
 
     rates = tuple([] for _ in range(no_assets))
@@ -266,48 +252,36 @@ def get_decreasing_rates(size: int = 20, no_assets: int = 10) -> Tuple[Sequence[
         for _ in range(size):
             each_rate.append(each_rate[-1] * .9)
 
-    return list(range(size)), rates
+    return ((i, x) for i, x in enumerate(rates))
 
 
-def get_crypto_rates(interval_minutes: int = 1) -> Tuple[Sequence[int], Sequence[Sequence[float]]]:
+def get_crypto_rates(interval_minutes: int = 1) -> Iterator[Tuple[int, Sequence[float]]]:
     pairs = (
         ("bcc", "eth"), ("bnb", "eth"), ("dash", "eth"), ("icx", "eth"),
         ("iota", "eth"), ("ltc", "eth"), ("nano", "eth"), ("poa", "eth"),
         ("qtum", "eth"), ("theta", "eth"), ("tusd", "eth"), ("xmr", "eth")
     )
     generator = merge_generator(pairs, interval_minutes=interval_minutes, header=("close_time", "close", ))
-
-    timestamps = []
-    rates = tuple([] for _ in pairs)
-
-    for data in generator:
-        assert None not in data
-        for i, (each_rates, (close_time, close_rate)) in enumerate(zip(rates, data)):
-            if i < 1:
-                timestamps.append(close_time)
-            each_rates.append(close_rate)
-
-        if Timer.time_passed(2000):
-            print(f"length of rates {len(timestamps):d}...")
-
-    return timestamps, rates
+    return ((snapshot[0][0], tuple(each_data[1] for each_data in snapshot)) for snapshot in generator)
 
 
 def main():
-    # timestamps, rates = get_decreasing_rates(no_assets=3, size=5)
-    timestamps, rates = get_random_rates(no_assets=3, size=5)
-    # timestamps, rates = get_crypto_rates(interval_minutes=1)
+    # timestamps, rates = zip(*get_decreasing_rates(no_assets=3, size=5))
+    # timestamps, rates = zip(*get_random_rates(no_assets=3, size=5))
+    timestamps, rates = zip(*get_debug_rates())
+    # timestamps, rates = zip(*get_crypto_rates(interval_minutes=1))
 
+    rates = list(zip(*rates))
     rates = [list(x) for x in rates]
-    rates[0][1] = -1.
-    rates[1][1] = -1.
-    rates[1][2] = -1.
-    rates[2][1] = -1.
+    #rates[0][1] = -1.
+    #rates[1][1] = -1.
+    #rates[1][2] = -1.
+    #rates[2][1] = -1.
     # rates.append([1. for _ in range(5)])
 
     size, = set(len(x) for x in rates)
 
-    path_trade, roi = forward_negative(rates, fees=fees_debug)
+    path_trade, roi = forward(rates, fees=fees_debug)
 
     if size < 30:
         print("tick    " + "".join(f"{i: 9d}" for i in range(size)))
@@ -317,6 +291,7 @@ def main():
         print()
         print("get     " + "".join([f"  ass_{x:03d}" for x in path_trade] + [f" {roi:8.2f} times investment returned"]))
 
+    exit()
     history_a, amount_a = simulate(path_trade, rates, objective_value=1., fees=fees_debug)
     # history_b, amount_b = simulate_alternative(path_trade, rates, objective_value=1., fees=fees_debug)
 
