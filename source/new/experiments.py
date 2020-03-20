2cef7aea63ba1066ef10073fd249d1a3015e87d6
Mark <wernsdorfer@gmail.com>
2020-03-20 16:10:35 +0100

    add visualization for regression

--- source/new/experiments.py
+++ source/new/experiments.py
@@ -1,7 +1,11 @@
+import queue
 from typing import Iterable, Sequence, Tuple, Generator, Union, Collection, Callable, Type, Any, TypeVar
 
+from matplotlib import pyplot
+
 from source.new.binance_examples import STATS, get_pairs
-from source.new.learning import Classification, MultivariateRegression, PolynomialClassification, RecurrentPolynomialClassification
+from source.new.learning import Classification, MultivariateRegression, PolynomialClassification, RecurrentPolynomialClassification, Approximation, smear, \
+    MultivariatePolynomialRegression, MultivariatePolynomialRecurrentRegression
 from source.new.optimal_trading import generate_multiple_changes
 from source.tools.timer import Timer
 
@@ -77,10 +81,10 @@ INPUT_VALUES = Sequence[float]
 TARGET_CLASS = int
 EXAMPLE = Tuple[INPUT_VALUES, TARGET_CLASS]
 
-INFO = Tuple[int, int, int, float, float, float, float]        # timestamp, output, target, error, knowledgeability, decidedness, roi
+INFO_INVESTMENT = Tuple[int, int, int, float, float, float, float]        # timestamp, output, target, error, knowledgeability, decidedness, roi
 
 
-def binance_time_series(classification: Classification, examples: Iterable[SNAPSHOT_BINANCE], names_assets: Sequence[str], fees: float) -> Generator[INFO, None, None]:
+def simulate_investment(classification: Classification, examples: Iterable[SNAPSHOT_BINANCE], names_assets: Sequence[str], fees: float) -> Generator[INFO_INVESTMENT, None, None]:
     amount_asset = 1.
     index_asset = -1
 
@@ -105,22 +109,93 @@ def binance_time_series(classification: Classification, examples: Iterable[SNAPS
             index_asset = index_target
 
         output_class = classification.output(rates)
+
         details = classification.get_details_last_output()
         output_raw = details["raw output"]
         error = MultivariateRegression.error(output_raw, tuple(float(i == index_target) for i in output_raw))
+        roi = amount_asset * rates[index_asset]
 
         classification.fit(rates, index_target, i + 1)
-
-        roi = amount_asset * rates[index_asset]
 
         yield timestamp, names_assets[output_class], names_assets[index_target], error, details["knowledgeability"], details["decidedness"], roi
 
 
-def save_info():
-    pass
+INFO_PREDICTION = Tuple[int, int, int, float, float]        # timestamp, output, target, error, knowledgeability, decidedness
 
 
-def main():
+def predict_rate(regression: Approximation[Sequence[float]], examples: Iterable[SNAPSHOT_BINANCE], names_assets: Sequence[str]) -> Generator[INFO_PREDICTION, None, None]:
+    last_snapshot = None
+    error_total = -1.
+
+    indices_rates = range(1, len(names_assets) + 1)
+    example_changes = extract_changes(examples, indices_rates)
+
+    for i, snapshot_changes in enumerate(example_changes):
+        if last_snapshot is None:
+            last_snapshot = snapshot_changes
+            continue
+        input_values = tuple(last_snapshot[i] for i in indices_rates)
+        output_values = regression.output(input_values)
+        target_values = tuple(snapshot_changes[i] for i in indices_rates)
+
+        error = MultivariateRegression.error(output_values, target_values)
+
+        timestamp = last_snapshot[0]
+        error_total = smear(error_total, error, i - 1)
+
+        regression.fit(input_values, target_values, i)
+
+        yield timestamp, output_values, target_values, error, error_total
+
+
+def learn_timeseries():
+    pairs = ("EOS", "ETH"),
+    stats = "close",
+
+    names_assets = tuple(f"{each_pair[0].upper():s}-{each_pair[1].upper()}" for each_pair in pairs)
+    columns = binance_columns(names_assets, stats)
+
+    examples = iterate_snapshots("../../data/examples/binance_examples.csv", columns, types_binance)
+
+    regression_non_recurrent = MultivariatePolynomialRegression(1, 4, 1)
+    simulation_non_recurrent = predict_rate(regression_non_recurrent, examples, names_assets)
+
+    regression_recurrent = MultivariatePolynomialRecurrentRegression(1, 4, 1)
+    simulation_recurrent = predict_rate(regression_recurrent, examples, names_assets)
+
+    fig, ax = pyplot.subplots()
+
+    times = queue.Queue(maxsize=100)
+    error_a = queue.Queue(maxsize=100)
+    error_b = queue.Queue(maxsize=100)
+
+    print_legend = True
+    # pyplot.ion()
+    for i, (snapshot_a, snapshot_b) in enumerate(zip(simulation_non_recurrent, simulation_recurrent)):
+        # print(snapshot)
+
+        if Timer.time_passed(1000):
+            print(f"finished reading {i:d} examples...")
+
+            times.put(i)
+            error_a.put(snapshot_a[-1])
+            error_b.put(snapshot_b[-1])
+
+            ax.clear()
+            t = list(times.queue)
+            ax.plot(t, list(error_a.queue), label="non recurrent", color="black", alpha=.5)
+            ax.plot(t, list(error_b.queue), label="recurrent", color="blue", alpha=.5)
+
+            if print_legend:
+                pyplot.legend()
+                print_legend = False
+            pyplot.pause(.05)
+
+            #print(print("non_recurrent: " + str(snapshot_a)))
+            #print(print("recurrent: " + str(snapshot_b)))
+
+
+def learn_investment():
     pairs = get_pairs()
 
     stats = STATS
@@ -134,9 +209,9 @@ def main():
     classification = RecurrentPolynomialClassification(len(pairs), 2, len(pairs))
 
     examples = iterate_snapshots("../../data/examples/binance_examples.csv", columns, types_binance)
-    time_series = binance_time_series(classification, examples, names_assets, .01)
+    simulation = simulate_investment(classification, examples, names_assets, .01)
 
-    for i, snapshot in enumerate(time_series):
+    for i, snapshot in enumerate(simulation):
         print(snapshot)
 
         if Timer.time_passed(2000):
@@ -144,4 +219,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # learn_investment()
+    learn_timeseries()
